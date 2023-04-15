f7b16e754679797617411cf2d2a78b7c069c0101
nmm6 <nmm6@hi.is>
2023-04-15 10:49:34 +0000

    Changes made to IntStackTest.java

--- src/test/java/is/hi/hbv202g/ass5/IntStackTest.java
+++ src/test/java/is/hi/hbv202g/ass5/IntStackTest.java
@@ -7,11 +7,58 @@ import org.junit.Test;
 
 public class IntStackTest {
 
+	private IntStack stack;
+	
+	@Before
+	public void setUp() throws Exception {
+		stack= new IntStack();
+	}
+	
     @Test
     public void testNewStackIsNotFull() {
-        IntStack stack= new IntStack();
 
         assertFalse(stack.isFull());
     }
+    
+    @Test
+    public void testFullStackIsFull() {
+    	for (int i = 0; i < stack.getCapacity(); i++) {
+    		stack.push(11);
+    	}
+    	assertTrue(stack.isFull());
+    }
+    
+    @Test
+    public void testStackIsFull() {
+    	for (int i = 0; i < stack.getCapacity(); i++) {
+    		assertFalse(stack.isFull());
+    		stack.push(11);
+    	}
+    }
+    
+    @Test
+    public void testPopReturnsPushedValue() {
+    	stack.push(32);
+    	stack.push(11);
+    	assertEquals(11, stack.pop());
+    	assertEquals(32, stack.pop());
+    }
 
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void testPopOnEmptyStack() {
+    	stack.pop();
+    }
+    
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void testPushOnFullStack() {
+    	try {
+    		for (int i = 0; i < stack.getCapacity(); i++) {
+    			stack.push(11);
+    		}
+    	} catch (Exception e) {
+    		fail("Exception occured but stack is not yet full.");
+    	}
+    	stack.push(11);
+    }
+    
 }
